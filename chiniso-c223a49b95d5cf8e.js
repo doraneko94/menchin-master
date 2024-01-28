9153bf1e80d1c9df12ae415b6d8e8d12e64a8c93
doraneko94 <shuntaro94@gmail.com>
2024-01-28 15:37:06 +0900

    change ogp

--- chiniso-c223a49b95d5cf8e.js
+++ chiniso-c223a49b95d5cf8e.js
@@ -686,11 +686,11 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper452 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper451 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 172, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper660 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper659 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 244, __wbg_adapter_27);
         return addHeapObject(ret);
     };
@@ -733,7 +733,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('chiniso-e589d2873e573085_bg.wasm', import.meta.url);
+        input = new URL('chiniso-c223a49b95d5cf8e_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
